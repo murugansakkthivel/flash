37d7431106c32e8716fe2f82255d81b91044c640
murugan <murugan@y-so-serious>
2018-11-14 07:24:25 +0530

    firstcommit

---
+++ summa/src/summa/Summa.java
@@ -0,0 +1,9 @@
+package summa;
+
+public class Summa {
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ summa/src/summa/inkeminkem.java
@@ -0,0 +1,10 @@
+package summa;
+
+public class inkeminkem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("marry christms");
+	}
+
+}
